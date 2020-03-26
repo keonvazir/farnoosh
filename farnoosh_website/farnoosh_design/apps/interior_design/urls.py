457ad91ed9f80d5a4d2005734134ca8ff562c300
keonvazir <54293518+keonvazir@users.noreply.github.com>
2020-03-26 11:01:35 -0700

    update

---
+++ farnoosh_website/farnoosh_design/apps/interior_design/urls.py
@@ -0,0 +1,10 @@
+########## APP LEVEL #############
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^about$', views.about),
+    url(r'^contact$', views.contact),
+    url(r'^projects$', views.projects),
+]
--- farnoosh_website/farnoosh_design/apps/interior_design/views.py
+++ farnoosh_website/farnoosh_design/apps/interior_design/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
-# Create your views here.
+def index(request):
+    return render(request, "interior_design/index.html")
+
+def about(request):
+    return render(request, "interior_design/about.html")
+
+def contact(request):
+    return render(request, "interior_design/contact.html")
+
+def projects(request):
+    return render(request, "interior_design/projects.html")
+    
+
+
