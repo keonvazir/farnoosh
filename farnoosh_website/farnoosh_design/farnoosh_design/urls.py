33d4a84f8a4a2f7ea155b4966a5646191094bf8b
keonvazir <54293518+keonvazir@users.noreply.github.com>
2020-03-26 21:25:49 -0700

    new

--- farnoosh_website/farnoosh_design/farnoosh_design/urls.py
+++ farnoosh_website/farnoosh_design/farnoosh_design/urls.py
@@ -14,7 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^', include('apps.interior_design.urls')),
-]
+] 
+
+
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
